fcaf83cf240e6fda256597e1136dbe38609150ab
rivit98 <albert98g@gmail.com>
2020-02-28 22:12:37 +0100

    zoom works now, split code into procedures, after 9h several bugs were fixed

--- zad2.asm
+++ zad2.asm
@@ -6,7 +6,7 @@ data1 segment
 	zoomvalue		db 			1
 	oldVideoMode	db			?
 
-	str_emptyArguments		db		"Uzycie: prog.exe ZOOMLEVEL ""tekst""",10,13,"$"
+	str_emptyArguments		db		"Uzycie: prog.exe zoomvalue ""tekst""",10,13,"$"
 	str_argumentsError		db		"Podane argumenty sa niepoprawne!",10,13,"$"
 	str_exitError			db		"Program zakonczyl sie bledem :(",10,13,"$"
 	str_success				db		"Plik zaszyfrowany pomyslnie!",10,13,"$"
@@ -30,15 +30,14 @@ start1:
 	int 10h
 	mov ds:[oldVideoMode], al
 
-	;call readArgs
+	call readArgs
 
 	;VIDEO - SET VIDEO MODE
 	mov ah, 0h
 	mov al, 13h			;13h = G  40x25  8x8   320x200  256/256K  .   A000 VGA,MCGA,ATI VIP
 	int 10h
 
-	call drawCharacter
-	;call displayText
+	call displayText
 
 	;czekaj na klawisz
 	;DOS 1+ - READ CHARACTER FROM STANDARD INPUT, WITH ECHO
@@ -148,8 +147,110 @@ start1:
 		ret
 	parseLastArg endp
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-	;procedura rysowania pojedynczego znaku, w al wymaga jaki znak rysowac
+	displayText proc
+		push ax
+		push cx
+		push di
+		push si
+
+		;przerobic to pozniej na mnozenie, mul
+		mov di, offset zoomvalue
+		mov ch, ds:[di]
+		xor ax, ax
+		loop_centering:
+			cmp ch, 0
+			je loop_centering_exit
+
+			add ax, 7h * 140h
+
+			dec ch
+			jmp loop_centering
+
+		loop_centering_exit:
+
+		xor di, di
+		mov di, 140h * 064h		;centruje tekst 320 * 100
+		;skoro czcionka jest wysoka na 14 to zanim zaczne rysowac musze odjac 7 * zoomvalue wierszy. czyli 7 * 320 * zoomvalue pixeli
+		sub di, ax				;ax wyliczone wczesniej
+		add di, 010h			;odsun troche tekst od lewej krawedzi, np o 16 pixeli
+
+
+		mov si, offset text
+		loop_drawLetters:
+			mov	al, ds:[si]		;znak tekstu
+
+			cmp al, 0			;tekst sie skonczyl
+			je drawingTextFinished
+
+			call drawCharacter
+
+			inc si
+
+			;zamiast 8 pixeli musimy sie przesunac o 8 * zoomvalue, bo znaki sa powiekszone
+			; add di, 8h
+			call increase_di
+
+			jmp loop_drawLetters
+
+		drawingTextFinished:
+
+		pop si
+		pop di
+		pop cx
+		pop ax
+		ret
+	displayText endp
+
+
+	;dodaje do di wartosc 8 * zoomvalue
+	increase_di proc
+		push si
+		push ax
+		push bx
+		push ds
+
+			mov bx, seg zoomvalue
+			mov ds, bx
+
+			xor bx, bx
+			mov si, offset zoomvalue
+			mov al, 8
+			mov bl, ds:[si]
+			mul bl
+			add di, ax
+
+		pop ds
+		pop bx
+		pop ax
+		pop si
+		ret
+	increase_di endp
+
+	indcrease_di_by_zoomvalue proc
+		push si
+		push ax
+		push ds
+		push bx
+
+			mov bx, seg zoomvalue
+			mov ds, bx
+
+			xor ax, ax
+			mov si, offset zoomvalue
+			mov al, ds:[si]
+			add di, ax
+
+		pop bx
+		pop ds
+		pop ax
+		pop si
+		ret
+	indcrease_di_by_zoomvalue endp
+
+
+	;procedura rysowania pojedynczego znaku, w al wymaga jaki znak rysowac, w ah wymaga zoomvalue, w di wymaga lewego gornego pixela (offset) od ktorego ma rysowac
 	drawCharacter proc
 		push es
 		push ds
@@ -159,6 +260,12 @@ start1:
 		push di
 		push si
 
+		xor ah, ah			;zostaw tylko dolne 8 bajtow (al)
+		mov bl, 0eh			;14 - bo tyle wierszy ma znak
+		mul bl				;mnoze ax (czyli teraz juz al) razy 14, bo jeden znak zajmuje 14bajtow
+		xor si, si			;si bedzie iteratorem po 14 bajtach jednego znaku
+		add si, ax			;offset bitmapy rysowanego znaku
+
 		;VIDEO - GET FONT INFORMATION (EGA, MCGA, VGA)
 		;bh - 02h ROM 8x14 character font pointer
 		;bh - 03h ROM 8x8 double dot font pointer
@@ -166,47 +273,34 @@ start1:
 		mov bh, 02h
 		int 10h
 
-		mov ax, es		;ustawiam datasegment, tam gdzie siedzą czcionki
+		mov ax, es		;ustawiam datasegment, tam gdzie sa czcionki
 		mov ds, ax
+
+		add si, bp		;bp jest zwracane przez powyzsze przerwanie, wskazuje na pamiec gdzie zaczynaja sie dane czcionek
+						;dodajemy ta wartosc do juz wyliczonego przez nas offsetu danego znaku
 
 		;tu zaczyna sie pamiec video
 		mov ax, 0a000h
 		mov es, ax
-		
-		xor si, si		;si bedzie iteratorem po 14 bajtach jednego znaku
-		add si, bp		;tu zaczynaja sie bitmapy czcionek, bp jest zwracane przez powyzsze przerwanie
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		xor ax, ax
-		mov al, 61h		;61h a
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 		;kazdy znak zajmuje 14bajtow czyli 112bity. Kazdy zapalony bit to pixel na ekranie. Kazdy wiersz to bajt.
 		;bajty 0-13 = znak ascii o indeksie 0
 		;14-27 = index 1
 		;.... itd
 
-		mov bl, 0eh
-		mul bl				;mnoze ax razy 16, bo jeden znak zajmuje 16bajtow
-		add si, ax			;po dodaniu si trzyma pierwszy bajt rysowanego znaku
-
-		xor di, di			;iterator po pamieci video 
-		; mov di, 320
-		; mul di, 064h		;to trzeba wyliczyc
-
 		mov ch, 0eh			;14 wierszy
 		loop_row:
 			cmp ch, 0
 			je drawingFinished
 
-			mov ah,	10000000b		;maska dla kolejnych bitow
+			mov bh,	10000000b		;maska dla kolejnych bitow
 			mov cl, 8				;8 kolumn
 			loop_column:
 				cmp cl, 0
 				je drawingColumnFinished
 
 				mov al, ds:[si]		;wyciag bajt czcionki
-				and al, ah			;sprawdzam czy dany pixel ma byc zapalony czy nie
+				and al, bh			;sprawdzam czy dany pixel ma byc zapalony czy nie
 
 				cmp al, 0			;jesli bit niezapalony - rysuj czarny kolor
 				je drawBlackPixel
@@ -216,24 +310,31 @@ start1:
 				jmp drawPixel
 
 				drawBlackPixel:
-					mov al, 00h			;czarny kolor
+					mov al, 0			;czarny kolor
 
 				drawPixel:
-					mov es:[di], al		;wpisz kolor na pamiec video
 
-					shr ah, 1h			;przesun maske w prawo (bo rysuje od lewej do prawej)
-					inc di
+					; mov es:[di], al		;rysuj pixel
+					; inc di
+
+					call drawSquares
+					call indcrease_di_by_zoomvalue
+
+					shr bh, 1			;przesun maske w prawo (bo rysuje od lewej do prawej)
 					dec cl
 
 				jmp loop_column
 
 			drawingColumnFinished:
-				add di, 320 		;przeskakujemy do nowego wiersza...
-				sub di, 8			;...ale jestesmy o 8 pixeli za daleko, wiec trzeba odjac
+				call nextRowBig			;przeskakujemy do nowego duzego wiersza... ale musimy to zrobic zoomvalue razy
+										;...ale jestesmy o 8 * zoomvalue pixeli za daleko, wiec trzeba odjac
+				
+				; add di, 320 		
+				; sub di, 8			
+				
 				inc si
 				dec ch
-			
-			jmp loop_row
+				jmp loop_row
 
 		drawingFinished:
 		pop si
@@ -245,6 +346,96 @@ start1:
 		pop es
 		ret
 	drawCharacter endp
+
+
+	;przesuwa di o zoomvalue wierszy
+	nextRowBig proc
+		push si
+		push cx
+		push bx
+		push ax
+		push ds
+
+		mov ax, seg zoomvalue
+		mov ds, ax
+
+		mov si, offset zoomvalue
+		mov cl, ds:[si]
+		loop_nextRowBig:
+			cmp cl, 0
+			je exitNextRowBig
+
+			add di, 320			;szerokosc ekranu
+
+			dec cl
+			jmp loop_nextRowBig
+
+		exitNextRowBig:
+
+		;teraz korekcja, cofnac sie trzeba o 8 * zoomvalue do tylu
+		xor ax, ax
+		mov al, ds:[si]
+		mov bl, 8
+		mul bl
+		sub di, ax
+
+		pop ds
+		pop ax
+		pop bx
+		pop cx
+		pop si
+		ret
+	nextRowBig endp
+
+
+	;rysuje duzy pixel zoomvalue x zoomvalue, wymaga w al koloru, di wskazuje na lewy gorny rog kwadratu
+	drawSquares proc
+		push di
+		push si
+		push cx
+		push ax
+		push bx
+		push ds
+
+		mov bx, seg zoomvalue
+		mov ds, bx
+
+		mov si, offset zoomvalue
+		mov ch, ds:[si]
+		loop_drawSquare1:
+			cmp ch, 0
+			je exitDrawSquare1
+
+			mov cl, ds:[si]
+			loop_drawSquare2:
+				cmp cl, 0
+				je exitDrawSquare2
+
+				mov es:[di], al
+
+				inc di
+				dec cl
+				jmp loop_drawSquare2
+
+			exitDrawSquare2:
+				add di, 320 		;przeskakujemy do nowego wiersza...
+				xor bx, bx
+				mov bl, ds:[si]
+				sub di, bx			;...ale jestesmy o zoomvalue pixeli za daleko, wiec trzeba odjac
+				dec ch
+				jmp loop_drawSquare1
+
+
+		exitDrawSquare1:
+
+		pop ds
+		pop bx
+		pop ax
+		pop cx
+		pop si
+		pop di
+		ret
+	drawSquares endp
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	;przesuwamy si, az napotkamy nie-spacje
